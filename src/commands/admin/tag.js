76997a259068c663124543556020c13b58363b00
Krampus B. <tiobarryx@gmail.com>
2024-12-15 22:05:38 +0100

    Actualizar tag.js

--- src/commands/admin/tag.js
+++ src/commands/admin/tag.js
@@ -1,18 +1,27 @@
-const { PREFIX } = require("../../config");
+const { PREFIX, ASSETS_DIR } = require("../../config");
+const path = require("path");
 
 module.exports = {
   name: "tag",
   description: "Para fijar mensajes",
   commands: ["tag", "t"],
   usage: `${PREFIX}hidetag motivo`,
-  handle: async ({ fullArgs, sendText, socket, remoteJid, sendReact }) => {
+  handle: async ({
+    fullArgs,
+    sendText,
+    sendImageFromFile,
+    socket,
+    remoteJid,
+    sendReact,
+  }) => {
     const { participants } = await socket.groupMetadata(remoteJid);
-
     const mentions = participants.map(({ id }) => id);
 
     await sendReact("📌");
 
-    await sendText(`> ╚════ KrampusBot ═════╝
-      !\n\n${fullArgs}`, mentions);
+    await sendImageFromFile(
+      path.join(ASSETS_DIR, "images", "tag.jpg"),
+      `> ╚════ KrampusBot ═════╝\n\n${fullArgs}`
+    );
   },
 };
