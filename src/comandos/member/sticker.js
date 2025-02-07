b3dc570c04668b620cd5c0122773743bf7c74d0a
Krampus B. <tiobarryx@gmail.com>
2025-02-07 16:55:00 +0100

    Crear sticker.js

---
+++ src/comandos/member/sticker.js
@@ -0,0 +1,81 @@
+const { PREFIX, TEMP_DIR } = require("../../krampus");
+const { InvalidParameterError } = require("../../errors/InvalidParameterError");
+const path = require("path");
+const fs = require("fs");
+const { toSticker } = require("@bochilteam/scraper");
+
+module.exports = {
+  name: "sticker",
+  description: "Crea stickers de imagen/gif/vídeo",
+  commands: ["s", "sticker"],
+  usage: `${PREFIX}sticker (etiqueta imagen/gif/vídeo) o ${PREFIX}sticker (responde a imagen/gif/vídeo)`,
+  handle: async ({
+    isImage,
+    isVideo,
+    downloadImage,
+    downloadVideo,
+    webMessage,
+    sendErrorReply,
+    sendPuzzleReact,
+    sendStickerFromFile,
+  }) => {
+    if (!isImage && !isVideo) {
+      throw new InvalidParameterError(
+        "ummm...Debes indicarme lo que quieres que convierta a sticker\n> Krampus OM bot"
+      );
+    }
+
+    const outputPath = path.resolve(TEMP_DIR, "output.webp");
+
+    if (isImage) {
+      const inputPath = await downloadImage(webMessage, "input");
+      const imageBuffer = fs.readFileSync(inputPath);
+
+      // Convertir a sticker
+      const stickerBuffer = await toSticker(imageBuffer, {
+        packname: "Operacion Marshall",
+        author: "Krampus OM bot",
+      });
+
+      fs.writeFileSync(outputPath, stickerBuffer);
+
+      await sendPuzzleReact();
+      await sendStickerFromFile(outputPath);
+
+      fs.unlinkSync(inputPath);
+      fs.unlinkSync(outputPath);
+    } else {
+      const inputPath = await downloadVideo(webMessage, "input");
+
+      const sizeInSeconds = 10;
+      const seconds =
+        webMessage.message?.videoMessage?.seconds ||
+        webMessage.message?.extendedTextMessage?.contextInfo?.quotedMessage
+          ?.videoMessage?.seconds;
+
+      if (seconds > sizeInSeconds) {
+        fs.unlinkSync(inputPath);
+        await sendErrorReply(
+          `¡ABUSADOR! Este video tiene más de ${sizeInSeconds} segundos. Envía un video más corto.`
+        );
+        return;
+      }
+
+      const videoBuffer = fs.readFileSync(inputPath);
+
+      // Convertir video a sticker
+      const stickerBuffer = await toSticker(videoBuffer, {
+        packname: "Operacion Marshall",
+        author: "Krampus OM bot",
+      });
+
+      fs.writeFileSync(outputPath, stickerBuffer);
+
+      await sendPuzzleReact();
+      await sendStickerFromFile(outputPath);
+
+      fs.unlinkSync(inputPath);
+      fs.unlinkSync(outputPath);
+    }
+  },
+};
