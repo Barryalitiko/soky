189b1840490102787bc6ca682e3b2782811015cd
Krampus B. <tiobarryx@gmail.com>
2025-02-07 06:56:53 +0100

    Crear sticker.js

---
+++ src/comandos/member/sticker.js
@@ -0,0 +1,78 @@
+const { PREFIX, TEMP_DIR } = require("../../krampus");
+const { InvalidParameterError } = require("../../errors/InvalidParameterError");
+const path = require("path");
+const fs = require("fs");
+const { exec } = require("child_process");
+
+module.exports = {
+name: "sticker",
+description: "Faço figurinhas de imagem/gif/vídeo",
+commands: ["s", "sticker"],
+usage: `${PREFIX}sticker (etiqueta imagen/gif/vídeo) o ${PREFIX}sticker (responde a imagen/gif/vídeo)`,
+handle: async ({
+isImage,
+isVideo,
+downloadImage,
+downloadVideo,
+webMessage,
+sendErrorReply,
+sendPuzzleReact,
+sendStickerFromFile,
+}) => {
+if (!isImage && !isVideo) {
+throw new InvalidParameterError(
+"👻 Krampus 👻 Debes marcar imagen/gif/vídeo o responder a una imagen/gif/vídeo"
+);
+}
+
+const outputPath = path.resolve(TEMP_DIR, "output.webp");
+
+if (isImage) {
+  const inputPath = await downloadImage(webMessage, "input");
+  exec(
+    `ffmpeg -i ${inputPath} -vf scale=-1:512 -metadata:s:Author="Krampus OM bot" -metadata:s:Pack="Operacion Marshall" ${outputPath}`,
+    async (error) => {
+      if (error) {
+        console.log(error);
+        fs.unlinkSync(inputPath);
+        throw new Error(error);
+      }
+      await sendPuzzleReact();
+      await sendStickerFromFile(outputPath);
+      fs.unlinkSync(inputPath);
+      fs.unlinkSync(outputPath);
+    }
+  );
+} else {
+  const inputPath = await downloadVideo(webMessage, "input");
+  const sizeInSeconds = 10;
+  const seconds =
+    webMessage.message?.videoMessage?.seconds ||
+    webMessage.message?.extendedTextMessage?.contextInfo?.quotedMessage
+      ?.videoMessage?.seconds;
+  const haveSecondsRule = seconds <= sizeInSeconds;
+  if (!haveSecondsRule) {
+    fs.unlinkSync(inputPath);
+    await sendErrorReply(
+      `👻 Krampus 👻 Este video tiene más de ${sizeInSeconds} segundos! Envia un video más corto!`
+    );
+    return;
+  }
+  exec(
+    `ffmpeg -i ${inputPath} -y -vcodec libwebp -fs 0.99M -filter_complex "[0:v] scale=-1:512,fps=12,pad=512:512:-1:-1:color=white@0.0,split[a][b];[a]palettegen=reserve_transparent=on:transparency_color=ffffff[p];[b][p]paletteuse" -metadata:s:Author="Krampus OM bot" -metadata:s:Pack="Operacion Marshall" -f webp ${outputPath}`,
+    async (error) => {
+      if (error) {
+        console.log(error);
+        fs.unlinkSync(inputPath);
+        throw new Error(error);
+      }
+      await sendPuzzleReact();
+      await sendStickerFromFile(outputPath);
+      fs.unlinkSync(inputPath);
+      fs.unlinkSync(outputPath);
+    }
+  );
+}
+},
+};
+
