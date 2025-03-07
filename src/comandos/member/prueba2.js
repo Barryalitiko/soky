f92966a16506d7bdba8d96990585403968d3766a
Krampus B. <tiobarryx@gmail.com>
2025-03-07 10:19:57 +0100

    Actualizar prueba2.js

--- src/comandos/member/prueba2.js
+++ src/comandos/member/prueba2.js
@@ -7,18 +7,20 @@ module.exports = {
   usage: `${PREFIX}botonprueba`,
   handle: async ({ socket, remoteJid, sendReply }) => {
     try {
-      const buttonsMessage = {
+      const buttons = [
+        { buttonId: "opcion_1", buttonText: { displayText: "Opción 1" }, type: 1 },
+        { buttonId: "opcion_2", buttonText: { displayText: "Opción 2" }, type: 1 },
+        { buttonId: "opcion_3", buttonText: { displayText: "Opción 3" }, type: 1 },
+      ];
+
+      const buttonMessage = {
         text: "Elige una opción:",
         footer: "Mensaje de prueba",
-        buttons: [
-          { buttonId: "opcion_1", buttonText: { displayText: "Opción 1" }, type: 1 },
-          { buttonId: "opcion_2", buttonText: { displayText: "Opción 2" }, type: 1 },
-          { buttonId: "opcion_3", buttonText: { displayText: "Opción 3" }, type: 1 },
-        ],
+        buttons: buttons,
         headerType: 1,
       };
 
-      await socket.sendMessage(remoteJid, buttonsMessage);
+      await socket.sendMessage(remoteJid, buttonMessage);
     } catch (error) {
       console.error("Error al enviar el mensaje con botones:", error);
       await sendReply("❌ Error al enviar el mensaje con botones.");
