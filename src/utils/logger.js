0891785f043da0cadc509c814e849765b5022bb4
Krampus B. <tiobarryx@gmail.com>
2025-03-15 23:01:39 +0100

    Update logger.js

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,11 +1,11 @@
 const { version } = require("../../package.json");
 
 exports.sayLog = (message) => {
-  console.log("\x1b[36m[KRAMPUS | TALK]\x1b[0m", message);
+  console.log("\x1b[36m[【﻿ｓｏｋｙ】 | TALK]\x1b[0m", message);
 };
 
 exports.inputLog = (message) => {
-  console.log("\x1b[30m[KRAMPUS | INPUT]\x1b[0m", message);
+  console.log("\x1b[30m[【﻿ｓｏｋｙ】 | INPUT]\x1b[0m", message);
 };
 
 exports.infoLog = (message) => {
@@ -13,15 +13,15 @@ exports.infoLog = (message) => {
 };
 
 exports.successLog = (message) => {
-  console.log("\x1b[5m\x1b[32m[KRAMPUS ༴༎ OM]\x1b[0m", message);
+  console.log("\x1b[5m\x1b[32m[【﻿ｓｏｋｙ】 ༴༎ OM]\x1b[0m", message);
 };
 
 exports.errorLog = (message) => {
-  console.log("\x1b[31m[KRAMPUS | ERROR]\x1b[0m", message);
+  console.log("\x1b[31m[【﻿ｓｏｋｙ】 | ERROR]\x1b[0m", message);
 };
 
 exports.warningLog = (message) => {
-  console.log("\x1b[33m[KRAMPUS | ADVERTENCIA]\x1b[0m", message);
+  console.log("\x1b[33m[【﻿ｓｏｋｙ】 | ADVERTENCIA]\x1b[0m", message);
 };
 
 exports.bannerLog = () => {
